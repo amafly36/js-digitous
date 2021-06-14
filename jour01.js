9218cf8d24e8370e9c0aacc8a0ba3d116128bef9
amafly36 <amadoudjouldebarry01@gmail.com>
2021-06-14 15:18:34 +0200

    exo-jour01-js

---
+++ jour01.js
@@ -0,0 +1,34 @@
+/* console.log("Hello world"); */
+
+/* var test = "My name is Amadou";
+
+console.log(test); */
+
+
+/* var name = "Amadou"
+console.log("Nice to meet you " + name);
+ */
+/* var testLength = "I'm very long !"
+console.log(testLength); */
+
+/* var food = "croissant is meh";
+console.log(food.replace("meh", "so good")); */
+
+/* var basic = "This is Cool";
+var basicUp = "This is Cool";
+var basicDown = "This is Cool";
+console.log(basic, basicUp.toUpperCase(), basicDown.toLowerCase()); */
+
+var word = "banana";
+var letters = word.split("");
+console.log(word);
+
+
+
+
+
+
+
+
+
+
