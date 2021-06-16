400f77bfca8d0541ff8d9958c1a7b9740ea27849
amafly36 <amadoudjouldebarry01@gmail.com>
2021-06-16 15:11:09 +0200

    exo-jour03-terminé

---
+++ jour03.js
@@ -0,0 +1,96 @@
+var fruits = ["mango", "lemon", "blueberry"]
+console.log(fruits);
+console.table(fruits);
+
+/* Exo-02 */
+
+var ingredients = ["eggs", "miilk", "butter"]
+console.log(ingredients[1]);
+console.log(ingredients.indexOf("butter"));
+
+/* Exo-03 */
+
+var objetcts = ["pen", "book", "lamp"]
+objetcts.unshift("chair")
+console.log(objetcts);
+
+objetcts.pop()
+console.log(objetcts);
+
+objetcts.push("laptop");
+console.log(objetcts);
+
+objetcts.slice();
+console.log(objetcts);
+
+/* Exo-04 */
+
+var numbers = [4, 10, 8, 12, 6]
+console.log(numbers.reverse());
+numbers.sort((a, b) => a - b);
+console.log(numbers);
+
+/* Exo-05 */
+
+/* var total = 0
+var limit = 10
+for (var i = 0; i => total; i++);
+console.log(total); */
+
+
+/* Exo-06 */
+
+let sentence = 'Hello Konexio !';
+let rev = sentence.split('').reverse().join('');
+console.log(rev);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
